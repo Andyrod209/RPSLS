158aac9d138befcd1097ec54487ad91dd99ffac7
Bryce Payne <brycepayne@macbook-pro.lan>
2022-01-06 15:20:38 -0600

    human gesture setup

--- human.py
+++ human.py
@@ -1,7 +1,25 @@
 from player import Player
 
 
-class Human:
+class Human(Player):
     def __init__(self):
         self.user_1 = input("What's your name? ")
-        self.score = 0 
+        super().__init__()
+
+    def gesture_options(self):
+        print(self.attack_list)
+        user_choice = input('select which attack you want to use: ')
+        if user_choice == 1:
+            self.user_attack = self.attack_list[0]
+        elif user_choice == 2:
+            self.user_attack = self.attack_list[1]
+        elif user_choice == 3:
+            self.user_attack = self.attack_list[2]
+        elif user_choice == 4:
+            self.user_attack = self.attack_list[3]
+        elif user_choice == 5:
+            self.user_attack = self.attack_list[4]
+        else:
+            print('invalid input')
+
+
--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from human import Human
 from player import Player 
 from ai import Ai
+from human import Human
 
 # player = Player()
-ai = Ai()
-ai.random_gesture_choice()
+# ai = Ai()
+# ai.random_gesture_choice()
+ 
+player1 = Human()
+
+player1.gesture_options()
+
+
