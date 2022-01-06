327a5a5842ea97e7993752dbc94967385b744829
andyrod <andyrod209@gmail.com>
2022-01-06 12:54:47 -0800

    for to add game and human functions to repo.

--- game.py
+++ game.py
@@ -4,3 +4,18 @@
 class Game:
     def __init__(self):
         pass
+
+    def rules(self):
+        pass
+    
+    def game_type(self):
+        pass
+
+    def battle(self):
+        pass
+
+    def round(self):
+        pass
+    
+    def display_winner(self):
+        pass
--- human.py
+++ human.py
@@ -1,7 +1,7 @@
-
+from player import Player
 
 
 class Human:
     def __init__(self):
         self.user_1 = input("What's your name? ")
-        
+        self.score = 0 
